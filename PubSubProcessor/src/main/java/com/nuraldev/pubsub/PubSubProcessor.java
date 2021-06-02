3e97d0ec8b119806d38aeb5a7469b3224e9c97d0
Rahul Dev <rahul.dev@theleanapps.com>
2021-06-03 00:22:53 +0530

    added maven plugin

--- PubSubProcessor/src/main/java/com/nuraldev/pubsub/PubSubProcessor.java
+++ PubSubProcessor/src/main/java/com/nuraldev/pubsub/PubSubProcessor.java
@@ -1,10 +1,16 @@
 package com.nuraldev.pubsub;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource({"classpath*:pubSubContext.xml"})
 public class PubSubProcessor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SpringApplication.run(PubSubProcessor.class, args);
 	}
 
 }
